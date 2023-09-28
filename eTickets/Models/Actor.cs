09ce34f20f2c5b2cd288b2eb2ab79bd089dc6104
EdwardWL666 <642841427@qq.com>
2023-09-28 22:55:38 +0800

    下拉框和搜索

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -8,14 +8,18 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name ="Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is Required")]
         public string ProfilePictureURL { get; set; }
 
 		[Display(Name = "Full Name")]
-		public string FullName { get; set;}
+        [Required(ErrorMessage = "Full Name is Required")]
+        [StringLength(50,MinimumLength =3,ErrorMessage = "Full Name must be 3 and 50 chars")]
+        public string FullName { get; set;}
 
 		[Display(Name = "Biography")]
-		public string Bio { get; set;}
+        [Required(ErrorMessage = "Biography is Required")]
+        public string Bio { get; set;}
 
-        public List<Actor_Movie> Actors_Movies { get; set;}
+        public List<Actor_Movie>? Actors_Movies { get; set;}
     }
 }
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,11 +1,14 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+builder.Services.AddScoped<IActorsService, ActorsService>();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
